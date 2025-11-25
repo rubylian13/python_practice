9c24da11dce3bb22225bef05d51f99fb1d2f4235
rubylian <rubylian5813@gmail.com>
2025-11-25 09:44:30 +0800

    portfolio1 - Text to Morse Code Converter

---
+++ 9_portfolio/9_1_text_to_morse_code_converter/main.py
@@ -0,0 +1,32 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--',
+    '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...',
+    ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.',
+    ' ': '/'
+}
+
+def text_to_morse(user_input):
+    morse_code = " "
+    text = user_input.upper()
+    for char in text:
+        if char in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char]
+        else:
+            morse_code += MORSE_CODE_DICT[' ']
+    return morse_code.strip()
+
+user_input = input("Please enter your text convert to Morse Code: ")
+morse_output = text_to_morse(user_input)
+
+print(f"Your text is {user_input}")
+print(f"Converting to Morse Code is {morse_output}")
